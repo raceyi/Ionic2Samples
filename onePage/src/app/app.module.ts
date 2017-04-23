8a25d8b8e7fe6968c094979198938c6150607426
raceyi <kalen75@naver.com>
2017-04-23 10:37:14 +0900

    remove unnecessary files in onePage

--- onePage/src/app/app.module.ts
+++ onePage/src/app/app.module.ts
@@ -1,19 +1,12 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+
 import { LoginPage } from '../pages/login/login';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage,
     LoginPage
   ],
   imports: [
@@ -22,10 +15,6 @@ import { LoginPage } from '../pages/login/login';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage,
     LoginPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
