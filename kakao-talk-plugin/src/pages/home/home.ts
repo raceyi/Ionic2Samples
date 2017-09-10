8778984343de0211407c091a71dc6cc3072d8d9a
raceyi <kalen75@naver.com>
2017-09-11 01:06:16 +0900

    add kakao-tak-plugin

---
+++ kakao-talk-plugin/src/pages/home/home.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { NavController ,Platform} from 'ionic-angular';
+import { AppAvailability } from '@ionic-native/app-availability';
+
+declare var KakaoTalk:any;
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController,
+              private platform:Platform,
+              private appAvailability: AppAvailability){
+  }
+
+  kakaologin(){
+      var scheme;
+      if(this.platform.is('android')){
+          scheme='com.kakao.talk';         
+      }else if(this.platform.is('ios')){
+          scheme='kakaotalk://';
+      }else{
+          console.log("unknown platform");
+      }   
+      console.log("ionic-scheme:"+scheme);
+          this.appAvailability.check(scheme).then(
+          ()=> {  // Success callback
+              console.log(scheme + ' is available. call KakaoTalk.login ');
+              KakaoTalk.login(
+                    (userProfile)=>{
+                        console.log("userProfile:"+JSON.stringify(userProfile));
+                        var id;
+                        if(typeof userProfile === "string"){
+                                id=userProfile;
+                        }else{ // humm... userProfile data type changes. Why?
+                                id=userProfile.id;
+                        }
+                        console.log('Successful kakaotalk login with'+id);
+                        /* !!! Add App server login here !!!*/
+                    },
+                    (err)=> {
+                        console.log('Error logging in');
+                        console.log(JSON.stringify(err));
+                        let reason={stage:"login_err",msg:err}; 
+                    }
+              ); 
+          },
+          (error)=>{  // Error callback
+              console.log("appAvailability error:"+JSON.stringify(error));
+          });
+  }
+}
