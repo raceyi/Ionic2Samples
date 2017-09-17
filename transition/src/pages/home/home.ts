f4d7f456eb3b4c2e3e2b0188103ee33852f8493d
raceyi <kalen75@naver.com>
2017-09-17 20:37:26 +0900

    fix backbutton action

--- transition/src/pages/home/home.ts
+++ transition/src/pages/home/home.ts
@@ -23,12 +23,12 @@ export class HomePage {
     //refer to https://github.com/driftyco/ionic/issues/6982. 
     //registerBackButtonAction에 대해 계속 업데이트되고 있음으로 위의 url을 참조하시기바랍니다. 
     this.platform.registerBackButtonAction(() => {
-
+            console.log("registerBackButtonAction");
             let activePortal = this.ionicApp._loadingPortal.getActive() ||
               this.ionicApp._modalPortal.getActive() ||
               this.ionicApp._toastPortal.getActive() ||
                 this.ionicApp._overlayPortal.getActive();
-        
+
             //activePortal is the active overlay like a modal,toast,etc
             if (activePortal) {
                 activePortal.dismiss();
@@ -38,12 +38,10 @@ export class HomePage {
                 this.menuCtrl.close();
                 return
             }
-
             let view = this.navController.getActive(); // As none of the above have occurred, its either a page pushed from menu or tab
             let activeVC = this.navController.getActive(); //get the active view
           
             let page = activeVC.instance; //page is the current view's instance i.e the current component I suppose
-                    
 
             if (!(page != this.viewCtrl)) { // Check if the current page is pushed from a menu click. 
                                             // HomePage와 동일한 페이지 인지를 확인합니다. 
@@ -52,7 +50,7 @@ export class HomePage {
                     this.navController.pop(); //pop if page can go back or if its an overlay over a menu page
                 }             
                 else {
-                          this.alertController.create({
+                         let alert =  this.alertController.create({
                                 title: '백키 버튼의 action이 없습니다.',
                                 buttons: [
                                     {
@@ -61,14 +59,11 @@ export class HomePage {
                                             this.platform.exitApp();
                                         }
                           }]});
+                          alert.present();
                 }
                 return;
-            }
-            //현재 페이지가 HomePage일 경우만 수행됩니다. 
-            let rootView = this.app.getActiveNav(); // So it must be a view from a tab. The current tab's nav can be accessed by this.app.getActiveNav();
-          
-            if (!rootView.canGoBack()) {
-                          this.alertController.create({
+            }else{            
+                 let alert = this.alertController.create({
                                 title: '앱을 종료하시겠습니까?',
                                 buttons: [
                                     {
@@ -79,11 +74,12 @@ export class HomePage {
                                     {
                                         text: '네',
                                         handler: () => {
+                                            console.log("terminate App");
                                             this.platform.exitApp();
                                         }
                           }]});
+                    alert.present();
             }
-            return rootView.pop();
         }, 0);
   }
 
