3f5d6b626628cccf837a296441d9f46959803376
raceyi <kalen75@naver.com>
2017-09-27 22:47:00 +0900

    add sample

---
+++ sample/src/pages/login/login.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  username:string;
+  password:string;
+
+  usernamePlaceHolder:string="아이디를 입력해주세요";
+  passwordPlaceHolder:string="비밀번호를 입력해주세요";
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+
+  login(){
+    console.log("username:"+this.username);
+    console.log("password:"+this.password); 
+  } 
+}
