62d12ccd0a2947308488da664067bbcd1d18ea46
raceyi <kalen75@naver.com>
2017-12-07 11:23:31 +0900

    update menu

--- menu/src/app/app.component.ts
+++ menu/src/app/app.component.ts
@@ -5,12 +5,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import { SettingPage } from '../pages/setting/setting';
+import { NavController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   rootPage:any = HomePage;
+  currentPage='HomePage';
 
   constructor(private platform: Platform, private app:App,
         statusBar: StatusBar, splashScreen: SplashScreen) {
@@ -22,9 +24,28 @@ export class MyApp {
     });
   }
 
+  checkPageHidden(page){
+    if(this.currentPage==page){
+        return true;
+    }
+    return false;
+  }
+
   openSettingPage(){
     console.log("openSettingPage");
     this.app.getRootNavs()[0].setRoot(SettingPage);
+    setTimeout(() => {  // workaround 
+      this.currentPage='SettingPage';   
+    }, 500); //  0.5 seconds      
+  }
+
+  openHomePage(){
+    console.log("openHomePage");
+    this.currentPage='HomePage';
+    this.app.getRootNavs()[0].setRoot(HomePage);
+    setTimeout(() => {  // workaround 
+      this.currentPage='HomePage';   
+    }, 500); //  0.5 seconds         
   }
 
   exit(){
