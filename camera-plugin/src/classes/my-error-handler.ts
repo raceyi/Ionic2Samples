d7d082bf21851eaaef4bd1df050e09ff8acd5b44
raceyi <kalen75@naver.com>
2017-12-13 20:20:32 +0900

    add my-error-handler into camera-handler

---
+++ camera-plugin/src/classes/my-error-handler.ts
@@ -0,0 +1,10 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicErrorHandler } from 'ionic-angular';
+
+ export class MyErrorHandler implements ErrorHandler {
+    handleError(error) {
+      // do something with the exception
+      console.log("error:"+error);
+    }
+ }
+
