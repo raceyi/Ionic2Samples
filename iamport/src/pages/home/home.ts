033472e503eea2512d1ec38a266649e47a64992a
raceyi <kalen75@naver.com>
2018-02-11 20:39:07 +0900

    update iamport

--- iamport/src/pages/home/home.ts
+++ iamport/src/pages/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient ,HttpHeaders} from '@angular/common/http';
 import { InAppBrowser } from '@ionic-native/in-app-browser';
 import { Platform,AlertController } from 'ionic-angular';
 import { HTTP } from '@ionic-native/http';
+import { BackgroundMode } from '@ionic-native/background-mode';
 
 declare var window:any;
 
@@ -15,21 +17,30 @@ export class HomePage {
   browserRef;
   done:boolean=false;
 
-  constructor(private platform: Platform,public navCtrl: NavController,private httpClient: HTTP,// It doesn't work under iOS. why? Hope it fixed soon. 
-    private iab: InAppBrowser,private alertController:AlertController) {
+  constructor(private platform: Platform,public navCtrl: NavController,
+            private backgroundMode:BackgroundMode,
+            private httpClient: HTTP,// It doesn't work under iOS. why? Hope it fixed soon. 
+            private iab: InAppBrowser,private alertController:AlertController) {
       console.log("HomePage constructor");
+      /*
+      platform.ready().then(() => {
+        this.backgroundMode.enable(); 
+      });
+      */
   }
 
   ionViewDidEnter(){
     console.log("ionViewDidEnter");
   }
 
-  getAccessToken(){
+    getAccessToken(){
     return new Promise((resolve,reject)=>{ 
-      let body={imp_key:"xxxxxxxxxxxxxx", imp_secret:"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"};
+      console.log("getAccessToken");
+      let body={imp_key:"XXXXXXX", imp_secret:"XXXXXXXXXXXXXXXXXX"};
       this.httpClient.post("https://api.iamport.kr/users/getToken",body,{}).then((res:any)=>{              
           console.log("res:"+JSON.stringify(res));
-          resolve(res.response.access_token);
+          let data=JSON.parse(res.data);
+          resolve(data.response.access_token);
       },(err)=>{
           console.log("err:"+JSON.stringify(err));
       });
@@ -37,13 +48,15 @@ export class HomePage {
   }
 
   paymentCreditCardAgain(){
+    console.log("paymentCreditCardAgain");
     this.getAccessToken().then((access_token:any)=>{
         console.log("access_token...:"+access_token);
-        let body={customer_uid:"your-customer-unique-id", 
-                  merchant_uid:'merchant_' + new Date().getTime(),  // 결제건별로 고유한 값을 지정합니다.
+        let headers = new HttpHeaders({Authorization:access_token});
+        let body={customer_uid:"your-customer-uid", 
+                  merchant_uid:"merchant-"+new Date().getTime(), 
                   amount:3300,
                   name:"iamport검증",
-                  vat:300 // 부가세 
+                  vat:300
                   //buyer_name:" ",
                   //buyer_email:" ",
                   //buyer_tel:" ",
@@ -54,13 +67,15 @@ export class HomePage {
                 };
         this.httpClient.post("https://api.iamport.kr/subscribe/payments/again",body,{Authorization:access_token}).then((res)=>{              
             console.log("res:"+JSON.stringify(res));
-            // "res:{"code":0,"message":null,
-            //       "response":{"amount":3300,"apply_num":"62748487","bank_code":null,"bank_name":null,"buyer_addr":null,"buyer_email":null,
-            //       "buyer_name":null,"buyer_postcode":null,"buyer_tel":null,"cancel_amount":0,"cancel_history":[],"cancel_reason":null,"cancel_receipt_urls":[],
-            //       "cancelled_at":0,"card_code":"361","card_name":"BC카드","card_quota":0,"cash_receipt_issued":false,"currency":"KRW","custom_data":null,"escrow":false,
-            //       "fail_reason":null,"failed_at":0,"imp_uid":"xxxxxxxx","merchant_uid":"xxxxxxxxx","name":"iamport검증","paid_at":1510653939,"pay_method":"card",
-            //       "pg_provider":"danal_tpay","pg_tid":"201711141905372583512400","receipt_url":"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-            //       "status":"paid","user_agent":"sorry_not_supported_anymore","vbank_code":null,"vbank_date":0,"vbank_holder":null,"vbank_name":null,"vbank_num":null}}"
+            if(res.status==200){
+                let data=JSON.parse(res.data);
+                if(data.code==0)
+                    console.log("success "+data.code);
+                else
+                    console.log("failure "+data.code+" message:"+data.message);                    
+            }else{
+                console.log("failure");
+            }
         },(err)=>{
             console.log("err:"+JSON.stringify(err));
         });
@@ -72,16 +87,16 @@ export class HomePage {
   registerCard(){
     let param={
             pay_method : 'card', // 'card'만 지원됩니다.
-            merchant_uid : 'merchant_' + new Date().getTime(), // 결제건별로 고유한 값을 지정합니다.
+            merchant_uid : 'merchant_' + new Date().getTime(),
             name : '최초인증결제',
-            amount : 0, // 빌링키 발급
-            customer_uid : 'your-customer-unique-id', //customer_uid 파라메터가 있어야 빌링키 발급을 시도합니다. 한번 등록된 값을 가지고 결제를 수행함으로 고객의 등록카드별로 다른값을 사용하시기 바랍니다.
+            amount : 0, // 빌링키 발급과 동시에 1,004원 결제승인을 시도합니다.
+            customer_uid : 'your-customer-uid', //customer_uid 파라메터가 있어야 빌링키 발급을 시도합니다.
             buyer_email : 'iamport@siot.do',
             buyer_name : '아임포트',
             buyer_tel : '02-1234-1234'
         }
 
-    const redirectUrl = "http://xxx.xxx.xxx.xxx/iamportResult";//It brings about load error. Please change it with your server address
+    const redirectUrl = "http://xxx.xxx.xxxx/oauth";//It brings about load error. Please change it with your server address
     let localfile;
     if(this.platform.is('android')){
         console.log("android");
