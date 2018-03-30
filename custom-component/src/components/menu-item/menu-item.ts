7d69122e9b8870c31a723802540b3120f5cc1b69
raceyi <kalen75@naver.com>
2018-03-30 18:56:49 +0900

    add ngOnInit into custom-components

--- custom-component/src/components/menu-item/menu-item.ts
+++ custom-component/src/components/menu-item/menu-item.ts
@@ -1,16 +1,20 @@
-import { Component ,Input,Output,EventEmitter} from '@angular/core';
+import { Component ,Input,Output,EventEmitter,OnInit} from '@angular/core';
 
 @Component({
   selector: 'menu-item',
   templateUrl: 'menu-item.html'
 })
-export class MenuItemComponent {
+export class MenuItemComponent implements OnInit {
 
   @Input('menu') menu;
   @Output('select') select = new EventEmitter();  
 
   constructor() {
     console.log('Hello MenuItemComponent Component');
+  }
+  
+  ngOnInit(){
+    console.log("menu:"+JSON.stringify(this.menu));
   }
 
   click(){
